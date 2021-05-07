7a64ab48922fbf918f08544675e58542b9f827a6
Jared Anders Isaksen <rayos.dev@gmail.com>
2021-05-07 06:20:31 +0200

    partly working

---
+++ src/DragDrop.js
@@ -0,0 +1,11 @@
+import React, { useRef, useState, forwardRef } from "react";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import "./styles.css";
+
+export default function DragDrop() {
+    return (
+        <>
+        
+        </>
+    )
+}
